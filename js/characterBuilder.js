1830aec82de84c415864886f3f153d1252c59453
Luke Breivik <luke@davai.fyi>
2024-02-07 18:02:00 -0500

    i forgot how to javascript

--- js/characterBuilder.js
+++ js/characterBuilder.js
@@ -2,5 +2,5 @@ import { Character } from "./character.js";
 import { questionList } from "./questionList.js";
 
 
-document.getElementById('container').innerHTML(questionList[0].getQuestionHTML());
+document.getElementById('container').innerHTML = questionList[0].getQuestionHTML();
 console.log(new Character().affinityValues);
