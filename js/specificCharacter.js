b5ffac1893102a239e0880401d4689283814885c
Luke Breivik <luke@davai.fyi>
2024-02-21 21:43:23 -0500

    do i have to put a real message on every single one of these

--- js/specificCharacter.js
+++ js/specificCharacter.js
@@ -1,5 +1,5 @@
 import { potentialCharacters } from './Metadata/potentialCharacters.js';
-import { sanitizeText } from './Metadata/sanitizeText.js';
+import { sanitizeText } from './links.js';
 let params = new URLSearchParams(window.location.search);
 let character = params.get('buildName');
 
