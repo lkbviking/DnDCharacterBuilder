c66b3d1961afa73421488ecbfece018f0b0cf0a4
Luke Breivik <luke@davai.fyi>
2024-02-21 19:38:37 -0500

    added link exports

--- js/links.js
+++ js/links.js
@@ -1,8 +1,8 @@
-function item(text) {
+export function item(text) {
     return `<a href="http://dnd5e.wikidot.com/wondrous-items:${sanitizeText(text)}">${text}</a>`;
 }
 
-function spell(text) {
+export function spell(text) {
     return `<a href="http://dnd5e.wikidot.com/spell:${sanitizeText(text)}">${text}</a>`;
 }
 
